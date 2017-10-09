de843791c1628fbc517efe56e8f6981d8e803f72
Kartik S Kulkarni <kartik.coolkarni@gmail.com>
2017-10-09 19:36:14 +0530

    Update Recharge.java

--- Paytm/src/test/java/com/Paytm/pages/Recharge.java
+++ Paytm/src/test/java/com/Paytm/pages/Recharge.java
@@ -54,7 +54,7 @@ public class Recharge extends BasePage{
 		mobile.click();
 		Thread.sleep(2000);
 		enterNo.clear();
-		enterNo.sendKeys("8951350157");
+		enterNo.sendKeys("1234567890");
 		Thread.sleep(2000);
 		operator.click();
 		driverMob.scrollToExact("Tata DOCOMO");
