396312a652042cef3952f39020e4327e11151fbb
Kartik S Kulkarni <kartik.coolkarni@gmail.com>
2017-10-09 19:32:43 +0530

    Update signUp.java

--- MooProjects/src/test/java/com/MooProjects/signUp.java
+++ MooProjects/src/test/java/com/MooProjects/signUp.java
@@ -14,7 +14,7 @@ public class signUp {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.flipkart.com/");
 		driver.findElement(By.xpath("//a[text()='Signup']"));
-		driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys("7892159224");
+		driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys("1234567890");
 		driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
 		WebElement otp=driver.findElement(By.xpath("(//input[@type='text'])[3]"));
 		
